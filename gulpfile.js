4efed53fb32f6d8056cd33482ae5d65cb77f35a6
Bahrul Uluma <bahrul221mb@gmail.com>
2017-11-17 22:38:26 +0530

    Add browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// plugins
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+/* gulp task untuk browser-sync */
+gulp.task('browser-sync', () => {
+    // ini adalah file2 yang akan didengar perubahannya
+    var files = [ '**/*.php', '**/*.css' ];
+    browserSync.init(files, {
+        // ganti sesuai dengan url project
+        proxy: 'localhost/learn-oop/'
+    });
+});
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Learn OOP</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        Learn OOP
+    </body>
+</html>
