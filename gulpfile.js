d751b7e26278516e5f2f87f2244fae6c85482b13
Bahrul Uluma <bahrul221mb@gmail.com>
2017-11-17 23:16:15 +0530

    add concat plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,37 @@
 // plugins
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+// tambahin dulu variable plugin nya
+var concat = require('gulp-concat');
+
 
 /* gulp task untuk browser-sync */
 gulp.task('browser-sync', () => {
     // ini adalah file2 yang akan didengar perubahannya
-    var files = [ '**/*.php', '**/*.css' ];
+    // ----
+    // tambahkan file js
+    var files = ['**/*.php', '**/*.css', 'js/**/*.js'];
     browserSync.init(files, {
         // ganti sesuai dengan url project
+        // -----
+        // ------ ganti nih ....
+        // -----
         proxy: 'localhost/php-oop/bahlul/'
     });
 });
+
+// task untuk gabungin script
+gulp.task('scripts', function () {
+    // file yang akan d gabung
+    var file = ['src/js/script-1.js', 'src/js/script-2.js']
+    return gulp.src(file)
+        // file hasil gabungan
+        .pipe(concat('script.js'))
+        // target folder
+        .pipe(gulp.dest('./js/'));
+});
+
+// task untuk memantau file dan menjalankan task scripts
+gulp.task('script-watch', ['scripts'], function () {
+    gulp.watch('src/js/**/*.js', ['scripts'])
+})
